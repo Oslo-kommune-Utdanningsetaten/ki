abd7f33c6e7e3cf04511cb99654be5016a168054
Thomas Drevon <thomas@drevon.no>
2024-11-07 09:51:53 +0100

    Show bot model

--- frontend/src/views/BotView.vue
+++ frontend/src/views/BotView.vue
@@ -254,7 +254,10 @@ watchEffect(() => {
       </div>
       <div class="speech-bubble-assistant position-relative bg-light p-3 border text-right">
         <strong>Dette er instruksene jeg har fått:</strong>
-        <p class="mb-0">{{ renderMessage(messages[0].content) }}</p>
+        <p>{{ renderMessage(messages[0].content) }}</p>
+        <p class="mb-0">
+          <strong>Jeg bruker modellen {{ bot.model }}.</strong>
+        </p>
       </div>
     </div>
   </div>
