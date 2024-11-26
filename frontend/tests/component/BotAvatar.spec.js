409d785533467f8ab7f4b0e08b34730fc99d86e9
Thomas Drevon <thomas@drevon.no>
2024-11-26 10:35:00 +0100

    Improve tests

---
+++ frontend/tests/component/BotAvatar.spec.js
@@ -0,0 +1,18 @@
+import { mount } from '@vue/test-utils'
+import BotAvatar from '../../src/components/BotAvatar.vue'
+import { sanitizeHtml } from '../testUtils.js'
+
+test('ensures a bot is rendered correctly', () => {
+  const avatarScheme = [3, 1, 0, 0, 0, 1, 1]
+
+  const wrapper = mount(BotAvatar, { props: { avatar_scheme: avatarScheme } })
+  const expected = `
+  <div hidden="">[3,1,0,0,0,1,1]</div>
+  <svg viewBox="0 0 12 18">
+    <g><rect class="oslo-fill-black" x="5" y="4" width="2" height="6"></rect></g><g><rect class="oslo-fill-red" x="2" y="0" width="8" height="4"></rect></g><g><rect class="oslo-fill-red" x="2" y="10" width="8" height="8"></rect></g><g><circle class="oslo-fill-black" cx="4" cy="2" r="1"></circle></g><g><circle class="oslo-fill-black" cx="8" cy="2" r="1"></circle></g><g><rect class="oslo-fill-black" x="2" y="10" width="2" height="6"></rect></g><g><rect class="oslo-fill-black" x="8" y="10" width="2" height="6"></rect></g>
+  </svg>
+`
+  const result = sanitizeHtml(wrapper.html())
+  expect(result).toContain(sanitizeHtml(expected))
+})
+
--- frontend/tests/component/BotAvatarSvgBodyPart.spec.js
+++ frontend/tests/component/BotAvatarSvgBodyPart.spec.js
@@ -1,9 +1,6 @@
 import { mount } from '@vue/test-utils'
 import BotAvatarSvgBodyPart from '../../src/components/BotAvatarSvgBodyPart.vue'
-
-// We dont care about exact formatting, just that the html is correct
-// Therefore, get rid of newlines and extra spaces
-const sanitizeHtml = (html) => html.split('\n').map(line => line.trim()).join('')
+import { sanitizeHtml } from '../testUtils.js'
 
 test('ensures eyes are rendered correctly', () => {
   const bodyPartProps = {
@@ -15,11 +12,11 @@ test('ensures eyes are rendered correctly', () => {
   }
 
   const wrapper = mount(BotAvatarSvgBodyPart, { props: { bodyPart: bodyPartProps } })
-  const expectedEyes = `
+  const expected = `
   <g><rect class="oslo-fill-dark-green" x="3" y="1" width="2" height="1"></rect></g>
   <g><rect class="oslo-fill-dark-green" x="7" y="1" width="2" height="1"></rect></g>
   `
-  const html = wrapper.html().split('\n').map(line => line.trim()).join('')
-  expect(sanitizeHtml(html)).toContain(sanitizeHtml(expectedEyes))
+  const result = sanitizeHtml(wrapper.html())
+  expect(result).toBe(sanitizeHtml(expected))
 })
 
