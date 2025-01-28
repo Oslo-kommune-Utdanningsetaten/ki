125a96eb0f9775a95c1ac1c241a3d797919cc4b1
Thomas Drevon <thomas@drevon.no>
2025-01-28 10:46:16 +0100

    ws urls as they should be going forward

--- frontend/src/components/BotCommunicationAudio.vue
+++ frontend/src/components/BotCommunicationAudio.vue
@@ -23,12 +23,9 @@ const props = defineProps({
   },
 })
 
-// const websocketUrl = import.meta.env.DEV
-//   ? 'ws://localhost:8000/ws/audio/'
-//   : `wss://${window.location.host}/ws/audio/`
-
-//const websocketUrl = 'ws://iz-ki-ap01t.oslo.int/ws/audio/'
-const websocketUrl = `ws://${window.location.host}/ws/audio/`
+const websocketUrl = import.meta.env.DEV
+  ? 'ws://localhost:8000/ws/audio/'
+  : `wss://${window.location.host}/ws/audio/`
 
 const isMicRecording = ref(false)
 const isBotSpeaking = ref(false)
