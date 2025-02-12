25c3a0ef62136b07b089ae3f56fd8db5eee59645
Thomas Drevon <thomas@drevon.no>
2025-02-12 16:07:20 +0100

    Add some very basic tests

---
+++ backend/ki/tests/test_hello.py
@@ -0,0 +1,7 @@
+import pytest
+from ki.utils import get_setting
+from ki.models import Setting
+
+def test_true_is_true():
+    """Simplest possible test to verify pytest is working"""
+    assert True == True
