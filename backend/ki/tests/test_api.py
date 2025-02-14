4e8c585f3a8b62e25d89703bf291597b071bb46d
Thomas Drevon <thomas@drevon.no>
2025-02-14 10:45:11 +0100

    Implement first API test

--- backend/ki/tests/test_api.py
+++ backend/ki/tests/test_api.py
@@ -1,24 +1,36 @@
 import pytest
-from rest_framework.test import APIClient
 from django.test import RequestFactory
+from ki.utils import get_setting
+from ki.models import Setting, BotModel
 
-from ki.views import menu_items
+from ki.views.api import menu_items
 
 def decorate_request(request):
     request.g = {}
     request.g['settings'] = {}
-    request.g['settings']['allow_groups'] = ['admin']
+    request.g['settings']['allow_groups'] = True
+    request.session = {'user.username': 'testuser'}
 
 
-@pytest.mark.django_db
-def test_menu_items_endpoint():
+@pytest.fixture
+def set_up_database(db, request):
+    Setting.objects.create(
+        setting_key='default_model',
+        label='Modell ved nye lærerboter',
+        is_txt=False,
+        int_val=1
+    )
+    BotModel.objects.create(
+        model_id=1,
+        deployment_id='gpt-4o-mini',
+        provider='Azure'
+    )
+
+
+@pytest.mark.django_db(reset_sequences=True)
+def test_menu_items_endpoint(set_up_database):
     """Test that the menu_items API endpoint returns status 200"""
-    factory = RequestFactory()
-    # Create a request and force authentication
-    request = factory.get('/api/menu_items')
+    request = RequestFactory().get('/api/menu_items')
     decorate_request(request)
-    
-    # Call the view with the request
     response = menu_items(request)
-
     assert response.status_code == 200
