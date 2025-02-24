5c5e6028d31d63d9372b12585d99f8b7e2fdad08
Thomas Drevon <thomas@drevon.no>
2025-02-24 11:34:25 +0100

    Remove redundant tests and add newline

--- backend/ki/tests/test_hello.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from ki.utils import get_setting
-from ki.models import Setting
-
-def test_true_is_true():
-    """Simplest possible test to verify pytest is working"""
-    assert True == True
