b76bc5dfdb8e51828861a8be5cb08d3e33d78006
Frode Nygård <frode.nygard@osloskolen.no>
2025-03-12 15:11:41 +0100

    fix tests

--- backend/ki/tests/test_api.py
+++ backend/ki/tests/test_api.py
@@ -6,8 +6,8 @@ from unittest.mock import patch
 
 
 
-def decorate_request(request, user_roles=[]):
-    request.g = {'settings': {'allow_groups': True}, 'dist_to_groups': True}
+def decorate_request(request, user_roles=[], user_groups=[]):
+    request.g = {'settings': {'allow_groups': True}, 'dist_to_groups': True, 'groups': user_groups}
     request.session = {'user.username': 'testuser'}
     for user_role in user_roles:
         request.g[user_role] = True
@@ -76,9 +76,9 @@ def test_empty_bot_endpoint_admin_response(set_up_database):
     """empty_bot endpoint returns something sane"""
     mock_groups = []
     # Avoid feide request and associated problems by mocking get_groups_from_request
-    with patch('ki.views.api.get_groups_from_request', return_value=mock_groups):
+    with patch('ki.utils.get_groups_from_g', return_value=mock_groups):
         request = RequestFactory().get('/api/empty_bot')
-        decorate_request(request, ['admin'])
+        decorate_request(request, ['admin'], [])
         response = empty_bot(request, '')
         assert response.status_code == 200
         assert response.data['bot']
@@ -89,7 +89,7 @@ def test_empty_bot_endpoint_employee_response(set_up_database):
     """empty_bot endpoint returns something sane"""
     mock_groups = []
     # Avoid feide request and associated problems by mocking get_groups_from_request
-    with patch('ki.views.api.get_groups_from_request', return_value=mock_groups):
+    with patch('ki.utils.get_groups_from_g', return_value=mock_groups):
         request = RequestFactory().get('/api/empty_bot')
         decorate_request(request, ['employee'])
         response = empty_bot(request, '')
@@ -102,7 +102,7 @@ def test_empty_bot_endpoint_author_response(set_up_database):
     """empty_bot endpoint returns something sane"""
     mock_groups = []
     # Avoid feide request and associated problems by mocking get_groups_from_request
-    with patch('ki.views.api.get_groups_from_request', return_value=mock_groups):
+    with patch('ki.utils.get_groups_from_g', return_value=mock_groups):
         request = RequestFactory().get('/api/empty_bot')
         decorate_request(request, ['author'])
         response = empty_bot(request, '')
